0e59ca2e5d8402b51cf66507f3f2de96900cfe9a
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2025-03-04 08:40:40 +0700

    update config db

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ const config = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT, // Thêm cổng database
+        port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT
     },
     test: {
@@ -16,16 +16,16 @@ const config = {
         password: process.env.DB_PASSWORD_TEST || null,
         database: process.env.DB_NAME_TEST || "database_test",
         host: process.env.DB_HOST_TEST || "127.0.0.1",
-        port: process.env.DB_PORT_TEST || 3306, // Cổng mặc định của MySQL
+        port: process.env.DB_PORT_TEST || 3306,
         dialect: process.env.DB_DIALECT_TEST || "mysql"
     },
     production: {
-        username: process.env.DB_USER_PROD || "root",
-        password: process.env.DB_PASSWORD_PROD || null,
-        database: process.env.DB_NAME_PROD || "database_production",
-        host: process.env.DB_HOST_PROD || "127.0.0.1",
-        port: process.env.DB_PORT_PROD || 3306, // Cổng mặc định của MySQL
-        dialect: process.env.DB_DIALECT_PROD || "mysql"
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT
     }
 };
 
